da77e96abc57afa9a9604a37c3184f3aa99b3c5b
zhengzhuang95 <zheng951203@163.com>
2021-07-02 17:12:10 +0800

    dist

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dist/',
+  // base: '/dist/',
+  base: process.env.NODE_ENV === 'production' ? '/vue3-element3-admin/dist/' : '/',
   plugins: [vue()]
 })
